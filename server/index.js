5388642f804f8f010857fa42ebb0f440f3249b2a
rakib hussain <rakibhussain8812@gmail.com>
2025-03-24 21:53:53 +0530

    g

--- server/index.js
+++ server/index.js
@@ -7,36 +7,26 @@ const {connectDb}=require("./config/connectDb")
 const {connectCloudinary}=require("./config/cloudinary")
 
 
-const authRoutes=require("./routes/auth")
-const itemRoutes=require("./routes/items")
-const resourceRoutes=require("./routes/resources")
-const chatRoutes=require("./routes/chat")
-
 
 const cookieParser = require("cookie-parser")
-const cors=require("cors")
-const fileupload=require("express-fileupload")
+// const cors=require("cors")
+// const fileupload=require("express-fileupload")
 
 require("dotenv").config()
 const PORT=process.env.PORT || 4000
 
 //middlewares
-app.use(fileupload({
-    useTempFiles:true,
-    tempFileDir:"/tmp"
-}))
+// app.use(fileupload({
+//     useTempFiles:true,
+//     tempFileDir:"/tmp"
+// }))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-    origin:"http://localhost:3000",
-    credentials:true
-}))
-
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/item",itemRoutes)
-app.use("/api/v1/resource",resourceRoutes)
-app.use("/api/v1/chat",chatRoutes)
+// app.use(cors({
+//     origin:"http://localhost:3000",
+//     credentials:true
+// }))
 
 
 //database connection
