d4a484d9ead3ef8fbe40257e2b54f7af2caa2d03
rakib hussain <rakibhussain8812@gmail.com>
2025-04-01 18:06:41 +0530

    user singup controller

--- server/controllers/authN.js
+++ server/controllers/authN.js
@@ -1,11 +1,44 @@
 //import the required models
-
+const User=require("../models/userModel")
+const bcrypt=require("bcrypt")
 // sign up of user
+exports.signUp=async(req,res)=>{
 
+    //fetch data
+    const {name,email,password,role,timestamps,status,profilePicture}=req.body
     //data validation 
+    if(!name || !email || !password || !role || !timestamps || !status || !profilePicture){
+        res.status(404).json({
+            success:false,
+            message:"please fill up all the details"
+        })
+    }
+        //check if the email already registered or not 
+    const user=User.findOne({email})
+    if(user){
+        res.status(403).json({
+            status:false,
+            message:"user already exists"
+        })
+    }
+
     //hash password 
+    const hashedPassword=bcrypt.hash(password,10)
     //save password 
+    const userInfo={
+        name,email,password:hashedPassword,role,timestamps,status,profilePicture
+    }
+    userInfo.save()
+
+    //send res
+    res.status(200).json({
+        success:true,
+        message:"User Registerd successfully",
+        data:userInfo
+    })
     //we may implement the concept of otp verificaiton here as well
+}
+
 
 //user login 
     //data validation
