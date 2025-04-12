bcf46eeb709b41397dd0f6aa456e07b09ef0cde1
rakib hussain <rakibhussain8812@gmail.com>
2025-04-13 01:58:41 +0530

    working in authz

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -3,7 +3,8 @@ export const axiosInstance=axios.create({
     baseURL: 'http://localhost:5000/api', // or your actual API URL
     withCredentials: true, // This enables cookie support
     headers: {
-        'Content-Type': 'application/json',
+        authorization:`Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json'
     },
 })
 
