2262b9406af7b42d5e5404921e5d275c0e376f83
rakib hussain <rakibhussain8812@gmail.com>
2025-04-17 21:39:29 +0530

    completed the loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { ColorRing } from 'react-loader-spinner'
 function Loader() {
   return (
-    <div>
-      <div>
+    <div className='fixed inset-0 bg-black z-10 flex justify-center items-center opacity-55'>
       <ColorRing
         visible={true}
         height="80"
@@ -13,8 +12,6 @@ function Loader() {
         wrapperClass="color-ring-wrapper"
         colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
       />
-      </div>
-      
     </div>
   )
 }
