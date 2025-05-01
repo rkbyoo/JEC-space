b34f5e8f24cac081800465a67d083f7df596c6cf
Abhishek Prasad <abhishekprasad1606@gmail.com>
2025-05-01 20:39:48 +0530

    Added correct email to contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -79,7 +79,7 @@ function Contact() {
           {/* Contact Info */}
           <div className="space-y-8">
             <InfoCard title="Get in Touch" items={[
-              { icon: Mail, text: 'jecSpace@gmail.com' },
+              { icon: Mail, text: 'jecspace1@gmail.com' },
               { icon: Phone, text: '+91 99992 39939' },
               { icon: MapPin, text: 'Jorhat Engineering College, Garmur, Jorhat, Assam. PIN-785007 ' },
             ]} />
