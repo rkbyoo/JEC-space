f5b2674eb21e69af56896483d6a2acb9e9288ea9
rakib hussain <rakibhussain8812@gmail.com>
2025-05-04 02:25:20 +0530

    updated the base_url for chatbot

--- client/src/services/botservices.jsx
+++ client/src/services/botservices.jsx
@@ -1,7 +1,7 @@
 // botservices.jsx
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000'; // Define your API base URL
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'; // Define your API base URL
 
 export const getBotResponse = async (message, mode, sessionId, availableProducts) => {
   try {
