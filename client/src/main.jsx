e44bafc6438342c7dedec42ede70fb6ad0ccd0df
rakib hussain <rakibhussain8812@gmail.com>
2025-05-04 01:22:29 +0530

    added anly

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,31 +1,32 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { App as AntdApp } from 'antd';
-import { ConfigProvider} from 'antd';
-import { Provider } from "react-redux";
-// import { StrictMode } from 'react'
-import store from "./redux/store";
-import { Analytics } from "@vercel/analytics/react"
-
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { App as AntdApp, ConfigProvider } from 'antd';
+import { Provider } from 'react-redux';
+import { StrictMode } from 'react';
+import store from './redux/store';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
-  <Analytics>
+  <StrictMode>
     <Provider store={store}>
-     <ConfigProvider theme={{
-      components:{
-        Button:{
-        colorPrimary:'#40513B',
-        colorPrimaryHover:"#40513B",
-        borderRadius:"2px"
-      }
-    }
-    }}>
-      <AntdApp>
-        <App></App>
-      </AntdApp>
-      
-    </ConfigProvider>
-   </Provider>
-   </Analytics>
-)
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              colorPrimary: '#40513B',
+              colorPrimaryHover: '#40513B',
+              borderRadius: '2px',
+            },
+          },
+        }}
+      >
+        <Analytics>
+          <AntdApp>
+            <App />
+          </AntdApp>
+        </Analytics>
+      </ConfigProvider>
+    </Provider>
+  </StrictMode>
+);
