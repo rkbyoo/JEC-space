106bb141f07f954325f59d6640cce53576fbe264
rakib hussain <rakibhussain8812@gmail.com>
2025-05-04 16:18:00 +0530

    added uploads file

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -2,7 +2,6 @@
 const User = require("../models/userModel")
 const Product = require("../models/productModel")
 const Notification = require("../models/notificationModel")
-const Offer = require("../models/offerModel")
 const bcrypt = require("bcrypt")
 const { uploadToCloudinary } = require("../utils/imageUploader")
 
@@ -77,45 +76,54 @@ exports.updateName = async (req, res) => {
 
 exports.deleteAccount = async (req, res) => {
   try {
-    //get the data from req
-    const { confirmPassword,userId} = req.body
-    //check the userid to be present in different databases and then delete them one by one
+    const { confirmPassword, userId } = req.body;
     if (!userId || !confirmPassword) {
       return res.status(404).json({
         success: false,
         message: "fetching user or password failed"
-      })
+      });
     }
-    const user = await User.findById(userId)
-    if (await bcrypt.compare(confirmPassword,user.password)) {
-      await Product.deleteMany({ seller: userId })
-      await Notification.deleteMany({ user: userId })
-      await Offer.deleteMany({seller:userId})
-      //delete the entry from user database
-      await User.findByIdAndDelete(userId)
-      //return res
-      return res.status(200).json({
-        success: true,
-        message: "The account is deleted successfully"
-      })
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
     }
-    else{
+    const isMatch = await bcrypt.compare(confirmPassword, user.password);
+    if (!isMatch) {
       return res.status(403).json({
-        success:false,
-        message:"The password is incorrect"
-      })
+        success: false,
+        message: "The password is incorrect"
+      });
     }
+
+    // 1. Delete all products by this user
+    await Product.deleteMany({ seller: userId });
+    // 2. Set product reference to null in all bids where product was deleted
+    await Bid.updateMany({ "product": { $in: await Product.find({ seller: userId }).distinct('_id') } }, { $set: { product: null } });
+    // 3. Set seller reference to null in all bids where seller is deleted
+    await Bid.updateMany({ seller: userId }, { $set: { seller: null } });
+    // 4. Set buyer reference to null in all bids where buyer is deleted
+    await Bid.updateMany({ buyer: userId }, { $set: { buyer: null } });
+    // 5. Delete all notifications by this user
+    await Notification.deleteMany({ user: userId });
+
+    // 7. Delete the user
+    await User.findByIdAndDelete(userId);
+
+    return res.status(200).json({
+      success: true,
+      message: "The account is deleted successfully"
+    });
   } catch (error) {
-    console.error("some error while deleting acc", error)
+    console.error("some error while deleting acc", error);
     return res.status(500).json({
       success: false,
       message: "internal server error in deleting acc"
-    })
+    });
   }
-}
-
-
-
+};
 
 exports.changePassword = async (req, res) => {
   try {
